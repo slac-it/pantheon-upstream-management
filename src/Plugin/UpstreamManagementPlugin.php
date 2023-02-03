77c9b9bb52831d36813d4a239dca2b0942aa8f65
Kevin Porras <kevin.porras@pantheon.io>
2023-02-03 13:41:39 -0600

    Remove commands from installing site.

--- src/Plugin/UpstreamManagementPlugin.php
+++ src/Plugin/UpstreamManagementPlugin.php
@@ -8,15 +8,50 @@ use Composer\Plugin\Capable;
 use Composer\Plugin\PluginInterface;
 use Composer\Composer;
 use Composer\IO\IOInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Script\ScriptEvents;
+use Composer\Script\Event;
+use PantheonSystems\UpstreamManagement\UpstreamManagementTrait;
 
 /**
  * Composer plugin to handle upstream management.
  */
-class UpstreamManagementPlugin implements PluginInterface, Capable
+class UpstreamManagementPlugin implements PluginInterface, Capable, EventSubscriberInterface
 {
+
+    use UpstreamManagementTrait;
 
     protected Composer $composer;
     protected IoInterface $io;
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ScriptEvents::POST_UPDATE_CMD => ['onPostUpdate', 100],
+        ];
+    }
+
+    public function onPostUpdate(Event $event)
+    {
+        $composerJsonContents = file_get_contents("composer.json");
+        $composerJson = json_decode($composerJsonContents, true);
+
+        if (isset($composerJson['scripts']['upstream-require'])) {
+            unset($composerJson['scripts']['upstream-require']);
+        }
+        if (isset($composerJson['scripts-descriptions']['upstream-require'])) {
+            unset($composerJson['scripts-descriptions']['upstream-require']);
+        }
+        if (isset($composerJson['scripts']['update-upstream-dependencies'])) {
+            unset($composerJson['scripts']['update-upstream-dependenciess']);
+        }
+        if (isset($composerJson['scripts-descriptions']['update-upstream-dependencies'])) {
+            unset($composerJson['scripts-descriptions']['update-upstream-dependencies']);
+        }
+
+        $composerJsonContents = $this->jsonEncodePretty($composerJson);
+        file_put_contents("composer.json", $composerJsonContents . PHP_EOL);
+    }
 
     public function activate(Composer $composer, IOInterface $io)
     {
@@ -39,7 +74,4 @@ class UpstreamManagementPlugin implements PluginInterface, Capable
     {
         return [CommandProvider::class => UpstreamManagementCommandProvider::class];
     }
-
-    // @todo Do we need preUpdate and postUpdate hooks!? Anything for cleanup?
-    // @todo Remove upstream-require command as part of the plugin
 }
